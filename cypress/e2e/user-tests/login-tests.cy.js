514077cfd1955ab0ceacb1ac2c0c1464cf642339
sslymn <slymann89@outlook.com>
2024-07-22 21:58:06 +0300

    GET,TYPE,TEXT,ASSERT

--- cypress/e2e/user-tests/login-tests.cy.js
+++ cypress/e2e/user-tests/login-tests.cy.js
@@ -1,9 +1,19 @@
-/// <reference types = "cypress" />
+describe('Login Test', () => {
 
-describe('Login Tests', () => {
-    
-    it("Login Succsessful",() => {
-        cy.visit('https://www.netflix.com/login');
+    it.only('Login Succsessful', () => {
+        cy.visit("https://www.netflix.com/login");
+        cy.get("[name='userLoginId']").type("denemehesabi299@outlook.com");
+        cy.get("[data-uia='password-field']").type("159753QA");
+        cy.get("[data-uia='login-submit-button']").click();
+        cy.get("[id='signOut']").should('have.text', "Oturumu Kapat");
     })
 
-});
+    it('Login Unsuccsessful', () => {
+        cy.visit("https://www.netflix.com/login");
+        cy.get("[name='userLoginId']").type("denemehesabi@outlook.com");
+        cy.get("[data-uia='password-field']").type("159753QA");
+        cy.get("[data-uia='login-submit-button']").click();
+        cy.get("[data-uia='error-message-container+header']").should('have.text', "Incorrect password for denemehesabi@outlook.com");
+    })
+
+})
